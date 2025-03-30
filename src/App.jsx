fcc6e7a70e23cda5ae314af75553647f3ae6c77a
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-03-30 21:01:00 +0330

    add buyCake

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import { Provider } from "react-redux";
-import "./App.css";
-import store from "./redux/store";
-import CakeContainer from "./components/CakeContainer";
-import MilkContainer from "./components/MilkContainer";
-import UsersList from "./components/UsersList";
-
+import { Provider } from 'react-redux'
+import './App.css'
+import store from './redux/store'
+import CakeContainer from './components/CakeContainer'
 function App() {
-  return (
-    <Provider store={store}>
-      <CakeContainer />
-      <MilkContainer />
-      <hr />
-      <UsersList />
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<CakeContainer />
+		</Provider>
+	)
 }
 
-export default App;
+export default App
--- src/redux/cake/cakeReducer.js
+++ src/redux/cake/cakeReducer.js
@@ -1,1 +1,17 @@
-export default function cakeReducer()
+import { BUY_CAKE } from './cakeTypes'
+
+const initialState = { numOfCakes: 10 }
+
+export default function cakeReducer(state = initialState, action) {
+	switch (action.type) {
+		case BUY_CAKE: {
+			return {
+				...state,
+				numOfCakes: state.numOfCakes - action.payload,
+			}
+		}
+
+		default:
+			return state
+	}
+}
